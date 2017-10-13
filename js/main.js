77d0e5f1de8a1ddcf8289971d32a22f2450cd015
Jae Sung Park <alberto.park@gmail.com>
2017-10-13 19:38:05 +0900

    update flow

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
+function checkTime() {
+	var d = new Date();
+	var hours = d.getHours();
+	var min = d.getMinutes();
+
+	return hours !== 13 &&
+		(hours >= 11 && hours <= 16) &&
+		(min >= 30 && min <= 59);
+}
+
 function generate() {
 	var code = document.getElementById('generate').value;
 
 	try {
 		window.c && window.c.destroy();
-		window.c = eval('('+ code +')');
-		alert("차트가 올바로 생성 되었다면, 부스의 진행요원 에게 보여주시고 추첨권을 받아가세요.");
-	} catch(e){
-		alert(e);
+		window.c = eval('(' + code + ')');
+	} catch(e) {}
+
+	if (checkTime()) {
+		if (c.internal.config.data_type === currentType) {
+			alert("짝짝~! 1차 미션을 성공하셨습니다. :)\n2차 미션을 이어서 수행해 주세요!");
+
+			setTimeout(function() {
+				location.href = "https://egjs.github.io/deview2017/test.html";
+			}, 1000);
+		} else {
+			alert("다시 시도해 주세요~!\n"+ currentType +" 유형의 차트를 생성해 주셔야 합니다.");
+		}
+	} else {
+		alert("죄송합니다. 아직 미션을 수행할 수 있는 시간이 아닙니다.\n아래의 시간 구간에서만 미션을 수행할 수 있습니다.\n\n11:30 ~ 11:59, 12:30 ~ 12:59, 14:30 ~ 14:59,\n15:30 ~ 15:59, 16:30 ~ 16:59")
 	}
 }
 
@@ -18,6 +39,16 @@ function generate() {
 		}
 	});
 
+	// chart type update per hour
+	document.getElementById("chart-type").innerHTML = window.currentType = ({
+		"t11": "bar",
+		"t12": "step",
+		"t14": "spline",
+		"t15": "area",
+		"t16": "donut"
+	})["t"+ (new Date().getHours())] || "bar";
+
+	// indicator
 	document.querySelector("#indicator p").innerHTML =
 		Array(f.getTotalCount() + 1).join("<span></span>");
 
@@ -26,5 +57,6 @@ function generate() {
 
 	dot[lastIndex].classList.add("on");
 
+	// code highlighting
 	hljs.initHighlightingOnLoad();
 }();
